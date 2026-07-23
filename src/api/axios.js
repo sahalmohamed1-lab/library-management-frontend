f1c0f6cf1d9dd48980d3d8acc98e2cb525937f97
Sahal Mohamed <sahal.mohamed1@student.moringaschool.com>
2026-07-24 02:15:39 +0300

    Deployed frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://library-management-backend-j1xz.onrender.com/api",
+  baseURL: "https://library-management-backend-j1xz.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
